93949936973a668ace8c064cef9eb8d591644ff7
Ilya Shchelkin <ilyashchelkin@mail.ru>
2024-07-24 16:25:27 +0400

    Добавлены код абстрактного выполнения программы

--- fs.go
+++ fs.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+)
 
+func main() {
+	rootPath, err := parseFlag()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	rootDirEntry, err := getRootDirEntry(rootPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	printTableDirEntries(rootDirEntry)
+}
+
+func parseFlag() (string, error) {
+	panic("unimplemented")
+}
+
+func getRootDirEntry(path string) ([]os.DirEntry, error) {
+	panic("unimplemented")
+}
+
+func printTableDirEntries(dirEntry []os.DirEntry) {
+	panic("unimplemented")
 }

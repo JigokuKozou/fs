ad900baf6f0df9370dfa5f6e5f7afd4155356f42
Ilya Shchelkin <ilyashchelkin@mail.ru>
2024-07-26 15:02:58 +0400

    Добавлено конфигурирование через файл .env

--- server.go
+++ server.go
@@ -1,32 +1,99 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
+
+// Config - конфигурация приложения
+type Config struct {
+	ServerPort string // Порт сервера
+}
+
+func init() {
+	// загружаем переменные .env в систему
+	if err := LoadEnvironmentVar(); err != nil {
+		log.Print("Файл .env не найден")
+	}
+}
 
 func main() {
 	defer func() {
 		if r := recover(); r != nil {
-			log.Fatalln("Произошла непредвиденная ошибка:", r)
+			log.Fatalln("произошла непредвиденная ошибка:", r)
 		}
 	}()
 
-	http.HandleFunc("/fs", fsHandler)
+	config, err := getConfig()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	server := &http.Server{
-		Addr:    fmt.Sprintf(":%s", "8080"),
+		Addr:    fmt.Sprintf(":%s", config.ServerPort),
 		Handler: http.DefaultServeMux,
 	}
 
-	err := server.ListenAndServe()
+	http.HandleFunc("/fs", fsHandler)
+
+	err = server.ListenAndServe()
 	if err != nil || errors.Is(err, http.ErrServerClosed) {
 		log.Fatalln(err)
 	}
+}
+
+// LoadEnvironmentVar загружает переменные окружения из файла .env
+func LoadEnvironmentVar() error {
+	file, err := os.Open(".env")
+	if err != nil {
+		return fmt.Errorf("ошибка открытия файла .env: %w", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// Пропуск пустых строк или коментариев
+		if len(line) == 0 || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		// Получения ключа и значения
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			return fmt.Errorf("неверный формат строки в .env файле: %s", line)
+		}
+
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+		os.Setenv(key, value)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("ошибка чтения файла .env: %v", err)
+	}
+
+	return nil
+}
+
+func getConfig() (Config, error) {
+	const (
+		httpServerPort = "HTTP_SERVER_PORT"
+	)
+	port, ok := os.LookupEnv(httpServerPort)
+	if !ok {
+		return Config{}, fmt.Errorf("переменная окружения %s не задана", httpServerPort)
+	}
+
+	return Config{
+		ServerPort: port,
+	}, nil
 }
 
 func fsHandler(w http.ResponseWriter, r *http.Request) {
@@ -40,12 +107,12 @@ func fsHandler(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
-			http.Error(w, "Директория не существует", http.StatusNotFound)
+			http.Error(w, "директория не существует", http.StatusNotFound)
 			log.Println(err)
 			return
 		}
 		if errors.Is(err, os.ErrPermission) {
-			http.Error(w, "Нет доступа к директории", http.StatusForbidden)
+			http.Error(w, "нет доступа к директории", http.StatusForbidden)
 			log.Println(err)
 			return
 		}

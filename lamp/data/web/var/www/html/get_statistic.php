975055a84c6cb464dc6822fa802d62d97f880da0
Ilya Shchelkin <ilyashchelkin@mail.ru>
2024-08-05 17:14:21 +0400

    Значения для графика теперь сортированы по возраставнию размера

--- lamp/data/web/var/www/html/get_statistic.php
+++ lamp/data/web/var/www/html/get_statistic.php
@@ -22,11 +22,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             }
         }
 
-        // Преобразование данных в JSON для использования в JavaScript
-        $jsonData = json_encode($data);
-
         // Генерация и вывод HTML-страницы
-        echo buildHtml($data, $jsonData);
+        echo buildHtml($data);
     } catch (Throwable $th) {
         header('Content-Type: application/json');
         http_response_code($th->getCode() ?: 500);
@@ -49,41 +46,50 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     ]);
 }
 
-function buildHtml($tableData, $chartJsonData)
+function tableDataToChartJson($tableData)
 {
-    return '<!DOCTYPE html>
-        <html lang="ru">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Статистика сканирования</title>'
-            . getStyles() .
-        '</head>
-        <body>
-            <canvas id="myChart" style="margin: 5px 5px 0 5px" ></canvas>
-            <h1>Статистика сканирования</h1>'
-            . buildTable($tableData) .
-            '<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-            <script>
-            const data = ' . $chartJsonData . ';
-            const ctx = document.getElementById("myChart").getContext("2d");
-            const labels = data.map(item => item.dirPath);
+    // Сортировка данных по возрастанию totalSize
+    usort($tableData, function ($a, $b) {
+        return $a['totalSize'] - $b['totalSize'];
+    });
+
+    // Преобразование данных в JSON для использования в JavaScript
+    return json_encode($tableData);
+}
+
+function buildHtml($tableData)
+{
+    $html = '<!DOCTYPE html>';
+    $html .= '<html lang="ru">';
+    $html .= '<head>';
+    $html .= '<meta charset="UTF-8">';
+    $html .= '<title>Статистика сканирования</title>';
+    $html .= getStyles();
+    $html .= '</head>';
+    $html .= '<body>';
+    $html .= '<canvas id="statChart"></canvas>
+            <h1>Статистика сканирования</h1>';
+    $html .= buildTable($tableData);
+    $html .= '<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>';
+    $html .= '<script>
+            const data = ' . tableDataToChartJson($tableData) . ';
             const totalSizes = data.map(item => item.totalSize);
             const loadTimes = data.map(item => item.loadTimeSeconds);
-            
+
             Chart.defaults.font.size = 16;
-            const myChart = new Chart(ctx, {
-                type: "scatter",
-                data: {
-                    labels: labels,
-                    datasets: [{
-                        label: "Отношение времени загрузки к размеру директории",
-                        data: totalSizes.map((size, index) => ({ x: size, y: loadTimes[index] })),
-                        backgroundColor: "rgba(75, 192, 192, 0.3)",
-                        borderColor: "rgba(75, 192, 192, 1)",
-                        borderWidth: 1
-                    }]
-                },
+            const chartData = {
+                datasets: [{
+                    label: "Время загрузки (сек)",
+                    data: totalSizes.map((size, index) => ({ x: size, y: loadTimes[index] })),
+                    backgroundColor: "rgba(75, 192, 192, 0.3)",
+                    borderColor: "rgba(75, 192, 192, 1)",
+                    borderWidth: 1
+                }]
+            };
+
+            const config = {
+                type: "line",
+                data: chartData,
                 options: {
                     responsive: true,
                     scales: {
@@ -91,7 +97,8 @@ function buildHtml($tableData, $chartJsonData)
                             title: {
                                 display: true,
                                 text: "Размер директории (байты)"
-                            }
+                            },
+                            type: "logarithmic"
                         },
                         y: {
                             title: {
@@ -101,10 +108,16 @@ function buildHtml($tableData, $chartJsonData)
                         }
                     }
                 }
-            });
-            </script>
-        </body>
-        </html>';
+            };
+
+            const ctx = document.getElementById("statChart").getContext("2d");
+            const myChart = new Chart(ctx, config);
+            </script>';
+
+    $html .= '</body>';
+    $html .= '</html>';
+
+    return $html;
 }
 
 function buildTable($array)
@@ -143,7 +156,6 @@ function getStyles()
             flex-direction: column;
             align-items: center;
             justify-content: flex-start;
-            height: 100vh;
             overflow: auto;
         }
         h1 {
@@ -155,17 +167,21 @@ function getStyles()
             max-width: 800px;
             background-color: #fff;
             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            margin-bottom: 20px;
         }
         th, td {
             border: 1px solid #ddd;
             padding: 4px;
-            text-align: center;
+            text-align: right;
         }
         th {
             background-color: #f2f2f2;
         }
         tr:nth-child(even) {
             background-color: #f9f9f9;
+        }
+        #statChart {
+            margin: 5px 5px 0 5px;
         }
     </style>';
 }

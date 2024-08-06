1e629c3cbf43155248262a130e3c766801a76630
Ilya Shchelkin <ilyashchelkin@mail.ru>
2024-08-06 12:07:10 +0400

    Удалён babel

--- webpack.config.js
+++ webpack.config.js
@@ -32,11 +32,6 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.js$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/,
-      },
-      {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
